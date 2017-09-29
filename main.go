b9d8de15fa807edb2cbdc776faab050859420e53
codingconcepts <robertreid@roberts-mbp.home>
2017-09-29 20:58:58 +0100

    Initial commit of MD5 and SHA algos

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"fmt"
+	"hash"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	algos := []algo{
+		algo{name: "MD5       ", hasher: md5.New()},
+		algo{name: "SHA1      ", hasher: sha1.New()},
+		algo{name: "SHA256    ", hasher: sha256.New()},
+		algo{name: "SHA256/224", hasher: sha256.New224()},
+		algo{name: "SHA512    ", hasher: sha512.New()},
+		algo{name: "SHA512/224", hasher: sha512.New512_224()},
+		algo{name: "SHA512/256", hasher: sha512.New512_256()},
+		algo{name: "SHA512/384", hasher: sha512.New384()},
+	}
+
+	if err := hashFile("README.md", algos...); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type algo struct {
+	name   string
+	hasher hash.Hash
+}
+
+func hashFile(fullPath string, algos ...algo) (err error) {
+	file, err := os.Open(fullPath)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	for _, a := range algos {
+		if _, err = io.Copy(a.hasher, file); err != nil {
+			return
+		}
+		digest := a.hasher.Sum(nil)
+		first, last := digest[0:3], digest[len(digest)-3:]
+		fmt.Printf("%s (%s) = %x...%x\n", a.name, filepath.Clean(fullPath), first, last)
+
+		if _, err = file.Seek(0, 0); err != nil {
+			return
+		}
+	}
+	return
+}
+
+/*
+	f, err := os.Open("file.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	h := md5.New()
+	if _, err := io.Copy(h, f); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%x", h.Sum(nil))
+*/
